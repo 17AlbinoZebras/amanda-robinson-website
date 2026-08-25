de011c1bd22d9ffe22e7a2cc8bf25b0f2c112409
17albinozebras <amanda@danivan.com>
2026-08-24 21:29:35 -0400

    Slightly adjust styling

--- src/app/resume.tsx
+++ src/app/resume.tsx
@@ -9,7 +9,7 @@ import { fas } from '@fortawesome/free-solid-svg-icons'
 export default function Resume() {
     return (
         <div className={styles.page}>
-            <TintedVector src="/masks/Red-Squiggles.svg" color='#D9E3DF' width='100vw' height='100vh' maskSize='cover' className={styles.resumeBackground}/>
+            <TintedVector src="/masks/Red-Squiggles.svg" color='#c7d6d1' width='100vw' height='100vh' maskSize='cover' className={styles.resumeBackground}/>
             <h1 className={styles.heading}>Resume</h1>
             <div className={styles.resume}>
                 <a className={styles.downloadButton} href='https://docs.google.com/document/d/1-O_EsqOcbVnY0tlRIP4p0EGaKX2AzlZMeZXaj3E4uS4/export?format=pdf'>< FontAwesomeIcon icon={fas.faDownload} /></a>
